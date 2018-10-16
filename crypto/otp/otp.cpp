81d0e070189e3853477d9b57c9b78dc081da9ff3
Gaurav Gogia <desmondmiles36@gmail.com>
2018-10-16 15:49:01 +0530

    CHANGES:
    1.Create dir crypto
    2.Create dir otp
    3. Create file otp.cpp

---
+++ crypto/otp/otp.cpp
@@ -0,0 +1,5 @@
+/*
+    One Time Pad implementation steps:
+    1. getKey(): a function for generating a string with len = len of message
+    2. encdec(): a function that will utilize XOR, take key and message to encrypt
+*/
